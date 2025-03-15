d01fa70dc3ba4eaa39264b76f7c629d5d35242c6
RodrigoRiboldi <rodrigorib41@gmail.com>
2025-03-15 17:59:18 -0300

    first commit

---
+++ app/cliente/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p> Pagina Cliente </p>
+      <Button>Este es el boton</Button>
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import NextTopLoader from 'nextjs-toploader';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PilarsaSRL",
+  description: "Pagina Web PilarsaSRL",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}> 
+        <NextTopLoader color="#000"/>
+        {children}
+      </body>
+    </html>
+  );
+}
